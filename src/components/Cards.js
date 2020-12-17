a02b12117dca116c39c11a2fc4cef5d9d55d3497
alexpryp <alexpryp@gmail.com>
2020-12-17 02:08:56 +0200

    React-based application with routing

---
+++ src/components/Cards.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Cards.css';
+import CardItem from './CardItem';
+
+function Cards() {
+  return (
+    <div className='cards'>
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/img-9.jpg'
+              text='Explore the paradise lands of the tropical belt'
+              label='Adventure'
+              path='/services'
+            />
+            <CardItem
+              src='images/img-2.jpg'
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
+              path='/services'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/img-3.jpg'
+              text='Visit the ancient towns of our beautiful planet'
+              label='Mystery'
+              path='/services'
+            />
+            <CardItem
+              src='images/img-4.jpg'
+              text='Take a journey through the megacities of the future'
+              label='Adventure'
+              path='/products'
+            />
+            <CardItem
+              src='images/img-8.jpg'
+              text='Take a look into the northern icy lands'
+              label='Adrenaline'
+              path='/sign-up'
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
